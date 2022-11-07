447232831625c4584755aa34d1f08931b3b747aa
Beatriz <beatrizsouzaf@hotmail.com>
2022-11-07 19:26:13 -0300

    delete course

--- teste2/script.js
+++ teste2/script.js
@@ -3,8 +3,8 @@ const openModal = () => document.getElementById('myModal').classList.add('active
 const closeModal = () => document.getElementById('myModal').classList.remove('active')
 
 const tempCourse = {
-    id: "5",
-    nome: "artes",
+    id: "6",
+    nome: "biologia",
     dataCriacao: "07/11/22",
     type: "tecnologo",
     ead: "true"
@@ -25,6 +25,12 @@ const readCourse = () => getLocalStorage()
 const updateCourse = (index, course) => {
     const dbCourse = readCourse()
     dbCourse[index] = course
+    setLocalStorage(dbCourse)
+}
+
+const deleteCourse = (index) => {
+    const dbCourse = readCourse()
+    dbCourse.splice(index, 1)
     setLocalStorage(dbCourse)
 }
 
