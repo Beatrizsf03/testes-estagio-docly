5f40a1a5d62f774b3c0c75216645ef641b321a46
Beatriz <beatrizsouzaf@hotmail.com>
2022-11-07 19:18:50 -0300

    create course

--- teste2/script.js
+++ teste2/script.js
@@ -2,6 +2,24 @@ const openModal = () => document.getElementById('myModal').classList.add('active
 
 const closeModal = () => document.getElementById('myModal').classList.remove('active')
 
+const tempCourse = {
+    id: "3",
+    nome: "ads",
+    dataCriacao: "07/11/22",
+    type: "tecnologo",
+    ead: "true"
+}
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_course')) ?? []
+const setLocalStorage = (dbCourse) => localStorage.setItem("db_course", JSON.stringify(dbCourse))
+
+//crud
+const createCourse = (course) => {  
+    const dbCourse = getLocalStorage()
+    dbCourse.push (course)
+    setLocalStorage(dbCourse)
+}
+
 //events
 document.getElementById('registerCourse').addEventListener( 'click', openModal )
 
