1f1ffa1d622cef7030681d74ee7dedcdeeb46c6f
Beatriz <beatrizsouzaf@hotmail.com>
2022-11-11 16:38:26 -0300

    teste3

---
+++ teste3/index.js
@@ -0,0 +1,45 @@
+const files = './teste3/documents'
+const fs = require('fs');
+
+fs.readdir(files, (err, files) => {
+        if (err) {
+            throw err;
+        } else {
+            console.log(files);
+        }
+    })
+
+fs.appendFile('arquivo-1.txt', 'arquivo-1', function (err) {
+    if (err) {
+        throw err;
+    }
+    console.log('Sucesso!');
+  });
+
+fs.appendFile('arquivo-2.txt', 'arquivo-2', function (err) {
+    if (err) {
+        throw err;
+    }
+    console.log('Sucesso!');
+  });
+
+fs.appendFile('arquivo-3.txt', 'arquivo-3', function (err) {
+    if (err) {
+        throw err;
+    }
+    console.log('Sucesso!');
+  });
+  
+fs.appendFile('arquivo-4.txt', 'arquivo-4', function (err) {
+    if (err) {
+        throw err;
+    }
+    console.log('Sucesso!');
+  }); 
+
+fs.appendFile('arquivo-5.txt', 'arquivo-5', function (err) {
+    if (err) {
+        throw err;
+    }
+    console.log('Sucesso!');
+  });
